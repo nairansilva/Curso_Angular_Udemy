efc5bc476086908087ca12e35e689f48c9fe67b9
Nairan Alves Silva <nairan.silva@totvs.com.br>
2021-06-08 18:39:55 -0300

    Finalizado Aula 2 módulo 7

--- 7 - Serviços/services-intro/src/app/app.module.ts
+++ 7 - Serviços/services-intro/src/app/app.module.ts
@@ -1,8 +1,13 @@
+import { Module1Module } from './module1/module1.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { Module2Module } from './module2/module2.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +15,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    Module1Module,
+    Module2Module,
+    MatToolbarModule,
+    MatSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
